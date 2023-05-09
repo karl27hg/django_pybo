e51b49ecd119885e6024a23f6ab32c715232118b
tkhan <karl27hg@gmail.com>
2023-05-09 12:01:22 +0900

    Add author function

--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -27,6 +28,7 @@ def detail(request, question_id):
     context = { 'question': question }
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """pybo 답변 등록"""
     question = get_object_or_404(Question, pk=question_id)
@@ -34,6 +36,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -45,12 +48,14 @@ def answer_create(request, question_id):
     # return redirect('pybo:detail', question_id=question.id)
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     """pybo 질문 등록"""
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
