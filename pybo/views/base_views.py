5e42510c1f0ea59d47651105cb7db02c5f00ee0a
tkhan <karl27hg@gmail.com>
2023-05-17 11:33:37 +0900

    Add search function

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -7,12 +8,20 @@ def index(request):
     """pybo 목록 출력"""
     # 입력 인자
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
     # 페이징 처리
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw }
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
