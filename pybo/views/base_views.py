cf8ed22696b37a81b16065fef0eb64ae1e179680
tkhan <karl27hg@gmail.com>
2023-05-18 16:16:32 +0900

    Add sorting function

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -9,8 +9,17 @@ def index(request):
     # 입력 인자
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
+    so = request.GET.get('so', 'recent')
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter = Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    # recent
+    else:
+        question_list = Question.objects.order_by('-create_date')
     # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |
